65f0388c1ce2321caa110e494eee7d623ff84600
Abiy Terefe Teshome <abiy.terefe@hotmail.com>
2019-03-13 21:23:52 +0100

    ch10-ex10.1/2

--- ch01/main.cpp
+++ ch01/main.cpp
@@ -13,9 +13,20 @@ int count (const Type& val, const std::list<Type>& vec)
 {
     return std::count(vec.begin(),vec.end(),val);
 }
+/*
+Exercise 10.1 :
+   The algorithm header defines a function named count that,
+   like find takes a pair of iterators and a value. count 
+   returns a count of how often that value appears. 
+   Read a sequence of ints into a vector and print count 
+   of how many elements have a give value.
+
+Exercise 10.2 :
+   Repeat the previous progarm but read values int list 
+   of strings.
+*/
 int main()
 {
-    std::cout<<"Hello WOrld!"<<std::endl;
     std::vector<int> v0{1,2,3,1,5,1,7,1,9};
     std::cout<<count(1,v0)<<'\n';
     std::list<std::string> str {"r","g","g","k"};
